d9e0b22514035a768242ae2d08e5e7918d6051dd
entro <entroxu@gmail.com>
2021-03-07 21:48:44 +0800

    源码

---
+++ spring-beans/src/test/java/org/springframework/beans/SpringBeans.java
@@ -0,0 +1,35 @@
+package org.springframework.beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import javax.sql.DataSource;
+
+public class SpringBeans {
+
+	/**
+	 * 基础容器，源码阅读Debug入口
+	 *
+	 */
+	@Test
+	public void test() {
+		//xml资源路径，todo, 找不到定位原因
+		String path = "test/beans.xml";
+		//创建 DefaultListableBeanFactory工厂，这就是spring的基础容器（另一个是高级容器）
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		//创建beanDefinition阅读器
+		XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+		//beanDefinition注册流程
+		beanDefinitionReader.loadBeanDefinitions(path);
+		//bean实例创建流程
+		DataSource dataSource = (DataSource) beanFactory.getBean("dataSource");
+
+		System.out.println(dataSource);
+	}
+
+	@Test
+	public void test1(){
+
+	}
+}
